b99fe2bee602abc37ab5ace1c9d2bcdf7d5f2a98
Alejandro Suarez <alejandrogsk9900@gmail.com>
2020-07-19 20:44:38 -0300

    docs agregado

---
+++ docs/precache-manifest.0c9d72e641ff0f6acf4e42e6bfeae969.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67e00b65069fd3c25a732dbc72f1dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d6ed345760e2621741c",
+    "url": "/static/css/main.28cfcafd.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6ed345760e2621741c",
+    "url": "/static/js/main.96de4ff1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
